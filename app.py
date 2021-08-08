46ebd867ef6440534fa15a666648dc99ae860d1f
Kristian Bonnici <kristiandaaniel@gmail.com>
2021-08-09 01:34:59 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/your-url', methods=['GET', 'POST'])
+def your_url():
+    if request.method == 'POST':
+        # code = shortened name for URL
+        return render_template('your_url.html', code=request.form['code'])
+    else:
+        return 'This is not valid'
