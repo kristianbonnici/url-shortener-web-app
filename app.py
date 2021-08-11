0fdf850475d24256e81e2989568a11925dce562f
Kristian Bonnici <kristiandaaniel@gmail.com>
2021-08-12 00:24:25 +0200

    +functionality: url shortener, file share, +data: json

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request
+from flask import redirect, url_for, flash, abort
+import json
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.secret_key = 'to_be_hidden_if_put_to_production'
 
 
 @app.route('/')
@@ -12,7 +17,62 @@ def home():
 @app.route('/your-url', methods=['GET', 'POST'])
 def your_url():
     if request.method == 'POST':
+        urls = {}
+
+        # Open JSON file, if it already exists
+        if os.path.exists('urls.json'):
+            with open('urls.json') as url_file:
+                urls = json.load(url_file)
+
+        # If trying to override existing code --> direct to home
+        if request.form['code'] in urls.keys():
+            flash("""That shortname has already been taken.
+                     Please select another name.""")
+            return redirect(url_for('home'))
+
+        # Check if URL or FILE upload by user
+        if 'url' in request.form.keys():
+            # ========== URL ==========
+            # key='code' : value={'url':'https://url'}
+            urls[request.form['code']] = {'url': request.form['url']}
+        else:
+            # ========== FILE ==========
+            f = request.files['input_file']
+            full_name = request.form['code'] + secure_filename(f.filename)
+
+            # save file to 'user_files' directory
+            f.save(os.getcwd() + '/static/user_files/' + full_name)
+            # key='code' : value={'file':'filename'}
+            urls[request.form['code']] = {'file': full_name}
+
+        # open/create JSON file & save user's input
+        with open('urls.json', 'w') as url_file:
+            json.dump(urls, url_file)
+
         # code = shortened name for URL
         return render_template('your_url.html', code=request.form['code'])
     else:
-        return 'This is not valid'
+        return redirect(url_for('home'))
+
+
+@app.route('/<string:code>')  # Redirect to URL or FILE
+def redirect_to_url(code):
+    if os.path.exists('urls.json'):
+        with open('urls.json') as urls_file:
+            urls = json.load(urls_file)
+            if code in urls.keys():
+                # If URL
+                if 'url' in urls[code].keys():
+                    return redirect(urls[code]['url'])
+                # Else FILE
+                else:
+                    return redirect(url_for('static', filename='user_files/'
+                                            + urls[code]['file']))
+
+    # If route not found --> return 404 PAGE
+    return abort(404)
+
+
+@app.errorhandler(404)  # route to our custom 404 PAGE
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
